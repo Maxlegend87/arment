14e8bce7381f28ae741ea24b7dcb9a9a76f6c381
Daniel Miniño Leyenda <maxlegend69@gmail.com>
2019-04-16 10:58:54 +0200

    Added show function

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ class Arment {
         this.definedArgs = {};
 
         this.definedArgsValues = {};
+
+        this.outputFunction = console.log;
     }
 
     get TYPES() { return TYPES; }
@@ -22,6 +24,8 @@ class Arment {
     get args() { return this.definedArgsValues; }
 
     set catch(func) { this.catchFunction = func; }
+
+    set output(func) { this.outputFunction = func; }
 
     add(name, keys, { optional = true, defaultValue, type = this.TYPES.ANY, desc = "", func = () => false } = {}) {
         if (!name || !keys || !keys.length) return this;
@@ -40,6 +44,59 @@ class Arment {
         func(value);
 
         return this;
+    }
+
+    show() {
+        let executionBasic = [];
+        let executionFull = [];
+        let stringDesc = "";
+        for (let name in this.definedArgs) {
+            let { keys, optional, defaultValue, type, desc } = this.definedArgs[name];
+
+            if (keys.some((key) => !isNaN(key))) {
+                if (optional) {
+                    executionBasic[keys[0]] = `[<${name}>]`;
+                    executionFull[keys[0]] = `[<${name}>]`;
+                } else {
+                    executionBasic[keys[0]] = `<${name}>`;
+                    executionFull[keys[0]] = `<${name}>`;
+                }
+                continue;
+            }
+
+            keys.sort((a, b) => a.length - b.length);
+
+            let keysMapped = keys.map((key) => (key.length === 1) ? `-${key}` : `--${key}`);
+            stringDesc += keysMapped.join(" ");
+            let forExec = keysMapped.join("|");
+
+            let typeDescriptor = "";
+            if (type !== this.TYPES.BOOLEAN) typeDescriptor = ` <${type}>`;
+            stringDesc += typeDescriptor;
+            forExec += typeDescriptor;
+
+            stringDesc += "\n";
+
+            if (optional) executionFull.push(`[${forExec}]`);
+            else {
+                executionBasic.push(forExec);
+                executionFull.push(forExec);
+            }
+
+            let defaultText = "";
+            if (defaultValue !== undefined && defaultValue !== null) {
+                defaultText = `[default: ${defaultValue}]`;
+            }
+            stringDesc += `\t\t\t${desc} ${defaultText}\n`;
+        }
+
+        let fullString = "Usage:\n";
+        fullString += `node file.js ${executionBasic.join(" ")} [options]\n`;
+        fullString += `node file.js ${executionFull.join(" ")}\n\n`;
+        fullString += "Options:\n";
+        fullString += stringDesc;
+
+        this.outputFunction(fullString);
     }
 }
 module.exports = new Arment();
